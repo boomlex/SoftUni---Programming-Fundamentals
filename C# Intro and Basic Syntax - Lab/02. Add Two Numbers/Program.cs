32eb5680706f3f5795adfddc770e72abebf669d1
boomlex <boomlexpower@gmail.com>
2017-10-19 00:53:38 +0300

    C# Intro and Basic Syntax - Lab / 100% Completed.

---
+++ C# Intro and Basic Syntax - Lab/02. Add Two Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02.Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int result = number1 + number2;
+
+            Console.WriteLine($"{number1} + {number2} = " + result);
+        }
+    }
+}
---
+++ C# Intro and Basic Syntax - Lab/03. Employee Data/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int id = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine("Employee ID: {0}", id.ToString("00000000"));
+            Console.WriteLine($"Salary: {salary:f2}");
+        }
+    }
+}
